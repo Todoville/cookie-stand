7af13184f1a8795cbfe6df867825e1e145f6bdff
Andrew Peacock <todoville@gmail.com>
2018-04-09 16:25:56 -0700

    finally starting to get somewhere, made a number apper in sales.html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-'use strict'
+'use strict';
+
+var pikePlaceMarket = {
+  hours : [],
+  minCustHour : 23,
+  maxCustHour : 65,
+  avgCookie : 6.3,
+};
+
+function renderCookies() {
+  var ulEl = document.getElementById('pike');
+  var liEl = document.createElement('li');
+  liEl.textContent = pikePlaceMarket.minCustHour;
+  ulEl.appendChild(liEl);
+}
+
+renderCookies();
