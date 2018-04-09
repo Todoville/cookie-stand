ec2674cdf7bdf2f65a3659fec3c82eeacec99581
Andrew Peacock <todoville@gmail.com>
2018-04-09 16:59:19 -0700

    random number generator is now generating random numbers

--- js/app.js
+++ js/app.js
@@ -1,17 +1,34 @@
 'use strict';
 
+function ranNo(min, max) {
+  min = Math.floor(min);
+  max = Math.ceil(max);
+  return Math.floor(Math.random() * (max-min)) + min;
+
+
+}
+
 var pikePlaceMarket = {
   hours : [],
   minCustHour : 23,
   maxCustHour : 65,
   avgCookie : 6.3,
+
+
+
+  renderCookies: function() {
+    var ulEl = document.getElementById('pike');
+    var liEl = document.createElement('li');
+    liEl.textContent = ranNo(this.minCustHour, this.maxCustHour);
+    ulEl.appendChild(liEl);
+  }
+
+
+
 };
 
-function renderCookies() {
-  var ulEl = document.getElementById('pike');
-  var liEl = document.createElement('li');
-  liEl.textContent = pikePlaceMarket.minCustHour;
-  ulEl.appendChild(liEl);
-}
+pikePlaceMarket.renderCookies();
 
-renderCookies();
+
+
+
